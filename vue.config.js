d8f71c0702da9ece64938b03288985f4df42b4dd
pippin <pippin@pippins-MacBook-Pro.local>
2023-11-18 15:21:31 +0100

    fix publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const publicPath = process.env.NODE_ENV === 'production' ? '/portfolio/' : '/'
+const publicPath = process.env.NODE_ENV === 'production' ? '/ViteStation3/' : '/'
 
 module.exports = {
  publicPath: publicPath,
