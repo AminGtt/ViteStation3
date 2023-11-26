e8c69ca1150e592a36839968633b86bbe09c6e1a
Amin Gatta <gatta.amin38@gmail.com>
2023-11-26 11:24:02 +0100

    Test compil audio assets

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script setup>
 
-
+import startupSound from "./assets/sounds/startup.mp3"
+import navSound from "./assets/sounds/nav.mp3"
 
 </script>
 
 <template>
 
-  <audio id="startup" src="./src/assets/sounds/startup.mp3" type="audio/mp3"></audio>
+  <audio id="startup" :src="startupSound" type="audio/mp3"></audio>
     
-  <audio id="nav" src="./src/assets/sounds/nav.mp3" type="audio/mpeg"></audio>
+  <audio id="nav" :src="navSound" type="audio/mpeg"></audio>
 
   <RouterView />
 </template>
